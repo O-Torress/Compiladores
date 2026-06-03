7e5714220fe7a256c491cc7d263d53e44ad3c3f4
Oscar Torres <166583974+O-Torress@users.noreply.github.com>
2026-06-02 22:45:41 -0400

    Implementar lectura de archivos en el analizador léxico y agregar archivo de prueba

--- analizador_lexico/src/main.rs
+++ analizador_lexico/src/main.rs
@@ -1,15 +1,31 @@
 use analizador_lexico::lexer::Lexer;
 use analizador_lexico::tokens::Token;
+use std::env;
+use std::fs;
 
 fn main() {
-    let un_codigo = "int x = 10;";
-    let mut lexer = Lexer::new(un_codigo);
-    println!("int x = 10;");
+    let args: Vec<String> = env::args().collect();
+    let archivo = if args.len() > 1 {
+        &args[1]
+    } else {
+        eprintln!("Uso: cargo run -- <hola.txt>");
+        std::process::exit(1);
+    };
+
+    let un_codigo = match fs::read_to_string(archivo) {
+        Ok(contenido) => contenido,
+        Err(err) => {
+            eprintln!("Error leyendo el archivo '{}': {}", archivo, err);
+            std::process::exit(1);
+        }
+    };
+
+    let mut lexer = Lexer::new(&un_codigo);
     println!("--- Iniciando Análisis Léxico ---");
     loop {
         let token = lexer.next_token();
         println!("{:?}", token);
-        
+
         if token == Token::EOF {
             break;
         }
